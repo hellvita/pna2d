9d8dbca426f319398528ac285a5f745466420018
hellvita <seredaolga03@gmail.com>
2026-03-07 23:55:08 +0200

    add input/output methods

--- src/interfaces/input.js
+++ src/interfaces/input.js
@@ -1,1 +1,13 @@
-export const readInput = () => {};
+import readline from 'node:readline/promises';
+import { stdin, stdout } from 'node:process';
+
+export const readInput = async () => {
+  const rl = readline.createInterface({ input: stdin, output: stdout });
+
+
+  const input = await rl.question('>> ');
+
+  rl.close();
+
+  return input;
+};
--- src/interfaces/output.js
+++ src/interfaces/output.js
@@ -1,1 +1,9 @@
-export const writeOutput = () => {};
+import start2dPA from '../app.js';
+
+export const writeOutput = async (result) => {
+  if (result) {
+    console.log(result);
+  }
+
+  await start2dPA();
+};
