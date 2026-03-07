7e0a79bc5843e077c8512dcd9e3af4bdcd39e645
hellvita <seredaolga03@gmail.com>
2026-03-07 23:59:09 +0200

    add the EXIT command

--- src/commands/constants.js
+++ src/commands/constants.js
@@ -1,7 +1,11 @@
 export const COMMANDS = {
   HELP: '--help',
+  EXIT: '--exit',
 };
+
+export const COMMANDS_LIST = Object.values(COMMANDS);
 
 export const DESCRIPTIONS = {
   [COMMANDS.HELP]: 'Show available input patterns',
+  [COMMANDS.EXIT]: 'Stop the CLI',
 };
