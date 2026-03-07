2af0dbeb2f1c9bccd8bdeb939778efc7facc0e1e
hellvita <seredaolga03@gmail.com>
2026-03-07 23:53:43 +0200

    add logic for the processInput method

--- src/services/processor.js
+++ src/services/processor.js
@@ -1,1 +1,14 @@
-export const processInput = () => {};
+import { normalizeInput } from '../normalizers/inputNormalizer.js';
+import { isCommand } from '../commands/service.js';
+import processCommand from '../commands/index.js';
+import processFigure from '../figures/index.js';
+
+export const processInput = (input) => {
+  const normalizedInput = normalizeInput(input);
+
+  if (isCommand(normalizedInput)) {
+    return processCommand(normalizedInput);
+  } else {
+    return processFigure(normalizedInput); // TODO
+  }
+};
