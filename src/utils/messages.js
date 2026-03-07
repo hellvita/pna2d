e89b5c14448db288454e6adbb8189d4ee96e9120
hellvita <seredaolga03@gmail.com>
2026-03-07 23:52:33 +0200

    add the printGreetings util

---
+++ src/utils/messages.js
@@ -0,0 +1,5 @@
+export const printGreetings = () => {
+  console.log(
+    '** Enter the 2D shape description following the pattern:\n\n[FIGURE_NAME] [FIGURE_COORDINATES] [ADDITIONAL_DATA]\n\ntype "--help" for more info\n',
+  );
+};
