80dd9bd49671e286bbe8d9fbe49516755e0b9bf4
hellvita <seredaolga03@gmail.com>
2026-03-08 23:10:08 +0200

    add the figure models

---
+++ src/figures/models.js
@@ -0,0 +1,202 @@
+import { FIGURES, VERTICES } from './constants.js';
+
+export class Circle {
+  #center = { x: null, y: null };
+  #radius = null;
+
+  calculatePerimeter() {
+    if (this.#radius === null) return null;
+
+    const perimeter = 2 * Math.PI * this.#radius;
+
+    return Number(perimeter.toFixed(2));
+  }
+
+  calculateArea() {
+    if (this.#radius === null) return null;
+
+    const area = Math.PI * this.#radius ** 2;
+
+    return Number(area.toFixed(2));
+  }
+
+  get name() {
+    return FIGURES.CIRCLE;
+  }
+
+  get center() {
+    return this.#center;
+  }
+  set center({ x, y }) {
+    if (typeof x === 'number' && typeof y === 'number') this.#center = { x, y };
+  }
+
+  get radius() {
+    return this.#radius;
+  }
+  set radius(radius) {
+    if (typeof radius === 'number' && radius >= 0) this.#radius = radius;
+  }
+}
+
+export class Rectangle {
+  #top = {
+    right: { x: null, y: null },
+    left: { x: null, y: null },
+  };
+  #bottom = {
+    right: { x: null, y: null },
+    left: { x: null, y: null },
+  };
+
+  #length = null;
+  #width = null;
+
+  #calculateVertices() {
+    if (
+      this.#top.right.x !== null &&
+      this.#top.right.y !== null &&
+      this.#bottom.left.x !== null &&
+      this.#bottom.left.y !== null
+    ) {
+      this.top.left.x = this.#bottom.left.x;
+      this.top.left.y = this.#top.right.y;
+
+      this.#bottom.right.x = this.#top.right.x;
+      this.#bottom.right.y = this.#bottom.left.y;
+    } else if (
+      this.#top.left.x !== null &&
+      this.#top.left.y !== null &&
+      this.#bottom.right.x !== null &&
+      this.#bottom.right.y !== null
+    ) {
+      this.#top.right.x = this.#bottom.right.x;
+      this.#top.right.y = this.#top.left.y;
+
+      this.#bottom.left.x = this.#top.left.x;
+      this.#bottom.left.y = this.#bottom.right.y;
+    }
+  }
+
+  #calculateSides() {
+    this.#calculateVertices();
+
+    const sideA = Math.abs(this.#top.left.y - this.#bottom.left.y);
+    const sideB = Math.abs(this.#bottom.right.x - this.#bottom.left.x);
+
+    if (sideA > sideB) {
+      this.#length = sideA;
+      this.#width = sideB;
+    } else {
+      this.#length = sideB;
+      this.#width = sideA;
+    }
+  }
+
+  calculatePerimeter() {
+    this.#calculateSides();
+
+    if (this.#length === null || this.#width === null) return null;
+
+    const perimeter = 2 * (this.#length + this.#width);
+
+    return Number(perimeter.toFixed(2));
+  }
+
+  calculateArea() {
+    this.#calculateSides();
+
+    if (this.#length === null || this.#width === null) return null;
+
+    const area = this.#length * this.#width;
+
+    return Number(area.toFixed(2));
+  }
+
+  setVertex({ vertexName, x = null, y = null }) {
+    if (x === null || typeof x !== 'number') return false;
+    if (y === null || typeof y !== 'number') return false;
+
+    switch (vertexName) {
+      case VERTICES.topRight: {
+        this.#top.right.x = x;
+        this.#top.right.y = y;
+
+        return true;
+      }
+      case VERTICES.topLeft: {
+        this.#top.left.x = x;
+        this.#top.left.y = y;
+
+        return true;
+      }
+      case VERTICES.bottomRight: {
+        this.#bottom.right.x = x;
+        this.#bottom.right.y = y;
+
+        return true;
+      }
+      case VERTICES.bottomLeft: {
+        this.#bottom.left.x = x;
+        this.#bottom.left.y = y;
+
+        return true;
+      }
+      default: {
+        return false;
+      }
+    }
+  }
+
+  get name() {
+    return FIGURES.RECTANGLE;
+  }
+
+  get top() {
+    return this.#top;
+  }
+  set top({ x, y }) {
+    if (typeof x === 'number' && typeof y === 'number') this.#top = { x, y };
+  }
+
+  get bottom() {
+    return this.#bottom;
+  }
+  set bottom({ x, y }) {
+    if (typeof x === 'number' && typeof y === 'number') this.#bottom = { x, y };
+  }
+}
+
+export class Square extends Rectangle {
+  #side;
+
+  calculatePerimeter() {
+    if (this.#side === null) return null;
+
+    const perimeter = 4 * this.#side;
+
+    return Number(perimeter.toFixed(2));
+  }
+
+  calculateArea() {
+    if (this.#side === null) return null;
+
+    const area = this.#side ** 2;
+
+    return Number(area.toFixed(2));
+  }
+
+  get name() {
+    return FIGURES.SQUARE;
+  }
+
+  get side() {
+    return this.#side;
+  }
+  setSide(side) {
+    if (side === null || typeof side !== 'number' || side < 0) return false;
+
+    this.#side = side;
+    return true;
+  }
+}
