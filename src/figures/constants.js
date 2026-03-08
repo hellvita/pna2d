ec72442b82b807259c970952266c711f7861762e
hellvita <seredaolga03@gmail.com>
2026-03-08 23:07:19 +0200

    add constants: PROMPT_EXAMPLES, VERTICES, METRICS

--- src/figures/constants.js
+++ src/figures/constants.js
@@ -3,3 +3,30 @@ export const FIGURES = {
   RECTANGLE: 'rectangle',
   CIRCLE: 'circle',
 };
+
+export const PROMPT_EXAMPLES = {
+  [FIGURES.SQUARE]: {
+    template: `${FIGURES.SQUARE} [vertex] [x] [y] side [s]`,
+    example: `${FIGURES.SQUARE} TopRight 1 1 Side 1`,
+  },
+  [FIGURES.RECTANGLE]: {
+    template: `${FIGURES.RECTANGLE} [vertex_1] [x_1] [y_1] [vertex_2] [x_2] [y_2]`,
+    example: `${FIGURES.RECTANGLE} TopRight 2 2 BottomLeft 1 1`,
+  },
+  [FIGURES.CIRCLE]: {
+    template: `${FIGURES.CIRCLE} center [x] [y] radius [r]`,
+    example: `${FIGURES.CIRCLE} center 1 1 radius 2`,
+  },
+};
+
+export const VERTICES = {
+  topRight: 'topright',
+  topLeft: 'topleft',
+  bottomRight: 'bottomright',
+  bottomLeft: 'bottomleft',
+};
+
+export const METRICS = {
+  PERIMETER: 'perimeter',
+  AREA: 'area',
+};
