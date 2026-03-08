11f2b24cc520a3e76eb5a9b57cc78df7355a4d3e
hellvita <seredaolga03@gmail.com>
2026-03-08 23:08:29 +0200

    update the printGreetings method, add the printFigureValidationError and the printCalculationError methods

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,5 +1,21 @@
+import { COMMANDS } from '../commands/constants.js';
+import { METRICS } from '../figures/constants.js';
+
 export const printGreetings = () => {
   console.log(
-    '** Enter the 2D shape description following the pattern:\n\n[FIGURE_NAME] [FIGURE_COORDINATES] [ADDITIONAL_DATA]\n\ntype "--help" for more info\n',
+    `\n** Enter a 2D shape description following the pattern:\n\n[FIGURE_NAME] [FIGURE_COORDINATES] [ADDITIONAL_DATA]\n\ntype '${COMMANDS.HELP}' for more info\n`,
   );
+};
+
+export const printFigureValidationError = (input) => {
+  console.log(
+    `\nThe description is invalid:\n\n${input}\n\ntype '${COMMANDS.HELP}' to see templates and examples\n`,
+  );
+};
+
+export const printCalculationError = ({ perimeter, area }) => {
+  if (!perimeter)
+    console.log(`\n(!) Failed to calculate ${METRICS.PERIMETER}\n`);
+
+  if (!area) console.log(`\n(!) Failed to calculate ${METRICS.AREA}\n`);
 };
