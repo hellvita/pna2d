6ebc45c0c55d11cd2e65064c0cf103cde7ef3c8f
hellvita <seredaolga03@gmail.com>
2026-03-09 13:17:51 +0200

    update commands

--- src/commands/constants.js
+++ src/commands/constants.js
@@ -1,11 +1,13 @@
 export const COMMANDS = {
-  HELP: '--help',
   EXIT: '--exit',
+  HELP: '--help',
+  PATTERNS: '--patterns',
 };
 
 export const COMMANDS_LIST = Object.values(COMMANDS);
 
 export const DESCRIPTIONS = {
-  [COMMANDS.HELP]: 'Show available input patterns',
+  [COMMANDS.HELP]: 'Show the list of all command available',
   [COMMANDS.EXIT]: 'Stop the CLI',
+  [COMMANDS.PATTERNS]: 'Show available input patterns',
 };
--- src/commands/service.js
+++ src/commands/service.js
@@ -1,8 +1,9 @@
-import { COMMANDS } from './constants.js';
+import { COMMANDS, DESCRIPTIONS } from './constants.js';
 import { PROMPT_EXAMPLES } from '../figures/constants.js';
 import { parseNestedObject } from '../utils/object.js';
 import { joinTitleWithDescription } from '../utils/string.js';
 import { stopInput } from '../interfaces/input.js';
+import { createTable } from '../utils/string.js';
 
 export const isCommand = (input) => {
   return input.startsWith('-');
@@ -10,17 +11,29 @@ export const isCommand = (input) => {
 
 export const executeCommand = (command) => {
   switch (command) {
-    case COMMANDS.HELP: {
-      const { keys, values } = parseNestedObject(PROMPT_EXAMPLES);
-
-      const helpMessage = joinTitleWithDescription(keys, values);
-
-      return helpMessage;
-    }
+    // ** ---- EXIT ---- ** //
     case COMMANDS.EXIT: {
       stopInput();
       break;
     }
+
+    // ** ---- HELP ---- ** //
+    case COMMANDS.HELP: {
+      const helpMessage = createTable(COMMANDS, DESCRIPTIONS, 'Command');
+
+      return helpMessage;
+    }
+
+    // ** ---- PATTERNS ---- ** //
+    case COMMANDS.PATTERNS: {
+      const { keys, values } = parseNestedObject(PROMPT_EXAMPLES);
+
+      const patternsMessage = joinTitleWithDescription(keys, values);
+
+      return patternsMessage;
+    }
+
+    // ** ---- DEFAULT ---- ** //
     default: {
       return null;
     }
